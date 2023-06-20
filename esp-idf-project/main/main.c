ab898b8c1e9f197c666e2e640c2eb8b136d4dfc9
GracePearcey <pearcey.grace@gmail.com>
2023-06-20 13:37:44 -0230

    cleanup and updates to readme

--- esp-idf-project/main/main.c
+++ esp-idf-project/main/main.c
@@ -7,7 +7,7 @@
 #include "bh_read_file.h"
 #include "bh_getopt.h"
 #include "bh_platform.h"
-#include "test_wasm_rs.h"
+
 #include <stdio.h>
 #include <inttypes.h>
 #include "sdkconfig.h"
@@ -15,7 +15,13 @@
 #include "freertos/task.h"
 #include "esp_log.h"
 
+/**
+ * WebAssembly App
+*/
+#include "test_wasm_rs.h"
+
 #define LOG_TAG "wamr_hello_world"
+#define NATIVE_STACK_SIZE (4*1024)
 
 int printHello(wasm_exec_env_t exec_env,int32_t number ){
     printf("Hello World #%ld \n", number);
@@ -76,8 +82,6 @@ void * iwasm_main(void *arg)
         ESP_LOGE(LOG_TAG, "Init runtime failed.");
         return NULL;
     }
-
-
     ESP_LOGI(LOG_TAG, "Run wamr with interpreter");
 
     wasm_file_buf = (uint8_t *)wasm_project_wasm;
@@ -87,7 +91,7 @@ void * iwasm_main(void *arg)
     if (!(wasm_module = wasm_runtime_load(wasm_file_buf, wasm_file_buf_size,
                                           error_buf, sizeof(error_buf)))) {
         ESP_LOGE(LOG_TAG, "Error in wasm_runtime_load: %s", error_buf);
-        goto fail2interp;
+        goto fail1interp;
     }
 
     ESP_LOGI(LOG_TAG, "Instantiate WASM runtime");
@@ -112,7 +116,7 @@ fail2interp:
     /* unload the module */
     ESP_LOGI(LOG_TAG, "Unload WASM module");
     wasm_runtime_unload(wasm_module);
-
+fail1interp:
 
     /* destroy runtime environment */
     ESP_LOGI(LOG_TAG, "Destroy WASM runtime");
